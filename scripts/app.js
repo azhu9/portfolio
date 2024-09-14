9fca60db72933d2d4cbfcad5d2e3834bf8489452
Andy Zhu <azhu625@gmail.com>
2024-09-14 17:58:43 -0400

    sticky sidebar

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,21 @@
 const toggler = document.querySelector(".toggler-btn");
+// const hiddenElements = document.querySelectorAll(".hidden");
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+     
+//     if(entry.isIntersecting){
+//       entry.target.classList.add("show");
+//     }
+//     else {
+//       entry.target.classList.remove("show");
+//     }
+//   });
+// });
+
 toggler.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("collapsed");
 });
+
+hiddenElements.forEach((el) => observer.observe(el));
